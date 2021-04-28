a670b2145b4c5c7af360db131c672a7c12e39124
Dan Ross <daniellross@gmail.com>
2021-04-28 15:46:27 -0500

    set up modalgs

--- src/components/category/CategoryList.js
+++ src/components/category/CategoryList.js
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useRef, useState } from "react"
 import { CategoryContext } from "./CategoryProvider"
 import "./Category.css"
 
 export const CategoryList = () => {
     const { categories, getCategories } = useContext(CategoryContext)
-
+    const deleteWarning = useRef()
+    const [deleteId, setDeleteId] = useState(0)
 
     useEffect(() => {
             getCategories()
@@ -14,17 +15,33 @@ export const CategoryList = () => {
     // filter out "deleted" categories
     const filteredCats = sortedCats.filter(cat => cat.deleted == 0)
 
-    const handleClickDelete = event => {
+    const handleDeleteWarning = event => {
         console.log(`clicked ${event.target.id}`)
+        deleteWarning.current.showModal()
+        setDeleteId(event.target.id)
     }
 
+    const handleCloseModal = () => {
+        setDeleteId(0)
+        deleteWarning.current.close()
+    }
+    
+    const handleClickDelete = () => {
+        console.log(deleteId)
+        deleteWarning.current.close()
+    }
     return (
         <>
             <div>
+            <dialog className="dialog dialog--auth" ref={deleteWarning}>
+                <div>Are you sure you want to delete this category?</div>
+                <button className="button--close" onClick={handleCloseModal}>Cancel</button>
+                <button className="button--close" onClick={handleClickDelete}>Confirm</button>
+            </dialog>
                 {filteredCats.map(c =>
                     <div className="category_card" key={c.id}>
                         <p><b>label: </b>{c.label}</p>
-                        <button id={c.id} onClick={handleClickDelete}>Delete</button>
+                        <button id={c.id} onClick={handleDeleteWarning}>Delete</button>
                     </div>
                 )}
             </div>
