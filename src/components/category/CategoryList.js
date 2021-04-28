95355d3794648d0b523bb34327c2277e67353335
Dan Ross <daniellross@gmail.com>
2021-04-28 16:07:41 -0500

    working on bu where each item on category list renders with the same id

--- src/components/category/CategoryList.js
+++ src/components/category/CategoryList.js
@@ -5,7 +5,10 @@ import "./Category.css"
 export const CategoryList = () => {
     const { categories, getCategories, deleteCategory } = useContext(CategoryContext)
     const deleteWarning = useRef()
-    const [deleteId, setDeleteId] = useState()
+    const [deleteCat, setDeleteCat] = useState({
+        "label": "",
+        "id": 0
+    })
 
     useEffect(() => {
         getCategories()
@@ -17,31 +20,36 @@ export const CategoryList = () => {
 
     const handleDeleteWarning = event => {
         deleteWarning.current.showModal()
-        setDeleteId(event.target.id)
+        
+        const category = categories.find(cat => cat.id = parseInt(event.target.id))
+        setDeleteCat(category)
     }
 
     const handleCloseModal = () => {
-        setDeleteId(0)
+        setDeleteCat({"label": "",
+        "id": 0})
         deleteWarning.current.close()
     }
 
     const handleClickDelete = () => {
-        deleteCategory(deleteId)
+        deleteCategory(deleteCat.id)
         handleCloseModal()
     }
+
+
 
     return (
         <>
             <div>
                 <dialog className="dialog dialog--auth" ref={deleteWarning}>
-                    <div>Are you sure you want to delete the category?</div>
+                    <div>Are you sure you want to delete the category "{deleteCat.label}"?</div>
                     <button className="button--close" onClick={handleCloseModal}>Cancel</button>
                     <button className="button--close" onClick={handleClickDelete}>Confirm</button>
                 </dialog>
                 {filteredCats.map(c =>
                     <div className="category_card" key={c.id}>
                         <p><b>label: </b>{c.label}</p>
-                        <button id={c.id} onClick={handleDeleteWarning}>Delete</button>
+                        <button key={`button--${c.id}`} id={c.id} onClick={handleDeleteWarning}>Delete</button>
                     </div>
                 )}
             </div>
